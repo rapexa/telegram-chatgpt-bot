7afabbebad64f6d09701b0a6478fcf70c2d0d2ac
rapexa <rapexamrs@gmail.com>
2025-01-30 04:13:51 -0800

    add some code for project implementations

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"telegram-chatgpt-bot.com/m/config"
+	"telegram-chatgpt-bot.com/m/database"
+	"telegram-chatgpt-bot.com/m/utils"
 )
 
 func main() {
 
 	// Load configuration
 	config.LoadEnv()
+
+	// Initialize logger
+	logger := utils.NewLogger()
+
+	// Connect to database
+	db, err := database.ConnectDB()
+	if err != nil {
+		logger.Fatal("Database connection failed:", err)
+	}
+	defer db.Close()
 
 }
