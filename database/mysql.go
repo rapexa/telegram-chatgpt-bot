8224e971a4bbaeadaae9816167b25e25215af0cd
rapexa <rapexamrs@gmail.com>
2025-01-30 04:13:28 -0800

    add mysql.go file for manageing database things and add logger for project logging

--- database/mysql.go
+++ database/mysql.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"telegram-chatgpt-bot.com/m/config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// ConnectDB initializes the database connection
+func ConnectDB() (*sql.DB, error) {
+	dsn := config.GetEnv("MYSQL_DSN")
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Database connected")
+	return db, nil
+}
